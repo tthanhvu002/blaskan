6d33235de623925fdd773a4b508b5e56dcd5508c
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2021-08-18 21:22:06 +0700

    upload

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+const mobileMenuBtn = document.querySelector('.mobile-menu');
+console.log(mobileMenuBtn);
+const mobileMenu  = document.querySelector('.mobile-menu__wrap');
+console.log(mobileMenu);
+
+mobileMenuBtn.onclick = function () {
+   
+    mobileMenu.classList.toggle('active')
+}
+
 const searchBtn = document.querySelector('.header__search-icon')
 const headerSearch = document.querySelector('.header__search');
 searchBtn.onclick = function () {
     headerSearch.classList.toggle('active');
 }
+
+const mobileSearchBtn = document.querySelector('.mobile-search__icon');
+const mobileSearch = document.querySelector('.mobile-search');
+
+mobileSearchBtn.onclick = function () {
+    mobileSearch.classList.toggle('active');
+}
+
